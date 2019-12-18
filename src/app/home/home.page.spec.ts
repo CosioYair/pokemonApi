c319376bedc37bfd8531af24045d7f194f8fa289
Yair Cosio <ycosio1310@gmail.com>
2019-12-17 18:26:57 -0600

    HomeComponent: Pokemon catch list

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,16 +1,43 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import { PokemonService } from '../shared/services/pokemon.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
+import { PokemonDetails } from '../shared/models/pokemon/pokemon-details';
 
-describe('HomePage', () => {
+fdescribe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  const pokemonName: string = 'pikachu';
+  const mockResponse: PokemonDetails = {
+    id: 1,
+    name: pokemonName,
+    sprites: {
+      back_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png',
+      back_female: '',
+      back_shiny: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/1.png',
+      back_shiny_female: '',
+      front_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+      front_female: '',
+      front_shiny: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png',
+      front_shiny_female: ''
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
+      declarations: [HomePage],
+      imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule.forRoot(),
+        HttpClientTestingModule
+      ],
+      providers: [PokemonService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
@@ -18,7 +45,25 @@ describe('HomePage', () => {
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
+  it('should create', async(inject([PokemonService], (_pokemonService: PokemonService) => {
     expect(component).toBeTruthy();
+  })));
+
+  describe('should catch the pokemon', () => {
+    it('should get results',
+      inject([HttpTestingController, PokemonService], (httpMock: HttpTestingController, _pokemonService: PokemonService) => {
+        _pokemonService.getPokemonDetails(pokemonName)
+          .subscribe(
+            (res) => {
+              expect(res).toEqual(mockResponse);
+            }
+          );
+        const req = httpMock.expectOne(`${environment.api}/pokemon/${pokemonName}`);
+        expect(req.request.method).toBe('GET');
+        req.flush(mockResponse);
+        fixture.detectChanges();
+      })
+    );
   });
+
 });
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PokemonService } from '../shared/services/pokemon.service';
+import { Pokemon } from '../shared/models/pokemon/pokemon';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public pokemons: Pokemon[] = [];
+  public pokemonName: string;
 
+  constructor(private _pokemonService: PokemonService) { }
+
+  catchPokemon() {
+    this._pokemonService.getPokemonDetails(this.pokemonName)
+      .pipe(
+        take(1)
+      )
+      .subscribe(pokemonDetail => {
+        const pokemon: Pokemon = {
+          name: pokemonDetail.name,
+          details: pokemonDetail
+        };
+        this.pokemons.push(pokemon);
+      });
+  }
 }
